67a0ecefa2d47799ef3bb8bbd51077a098a4826e
Russell Haering <russell_h@osuosl.org>
2010-04-30 20:31:17 -0700

    Trying to add some make.conf support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,20 @@
 class gentoo {
-    $modbase = "puppet:///modules/gentoo"
+    $modbase        = "puppet:///modules/gentoo"
+    $updateconfig   = "/usr/local/sbin/update-config"
+    $makeconf_d     = "/etc/make.conf.d"
+    $makeconf       = "/etc/make.conf"
 
-    exec { "sync-overlays":
-        path    => "/usr/local/sbin/:/usr/bin",
-        cwd     => "/tmp",
-        returns => 0,
-        require => [File["/etc/sync-overlays.cfg"],
-                    File["/usr/local/sbin/sync-overlays"],];
+    exec {
+        "sync-overlays":
+            path    => "/usr/local/sbin/:/usr/bin",
+            cwd     => "/tmp",
+            returns => 0,
+            require => [File["/etc/sync-overlays.cfg"],
+                        File["/usr/local/sbin/sync-overlays"],];
+        "update-makeconf":
+            command     => "${updateconfig} ${makeconf} ${makeconf_d} conf",
+            require     => File["${updateconfig}"],
+            refreshonly => true;
     }
 
     file {
@@ -20,18 +28,35 @@ class gentoo {
             ensure      => present,
             source      => "${modbase}/usr/local/sbin/sync-overlays",
             mode        => 755;
+        "${updateconfig}":
+            ensure      => present,
+            source      => "${modbase}${updateconfig}",
+            mode        => 755;
+        "/usr/local/overlays":
+            ensure      => directory;
+        "/etc/overlays":
+            ensure      => directory;
+        "${makeconf_d}":
+            ensure      => directory;
     }
 
-    file {"/usr/local/overlays": ensure => directory}
-    file {"/etc/overlays": ensure => directory}
+    gentoo::overlay {"osl":}
 
-    # Puppet went all nazi with its syntax here...
-    overlay {"osl":}
-}
+    gentoo::makeconf_include {"00base":}
 
-define overlay() {
-    file { "/etc/overlays/${name}":
-        ensure  => file,
-        before  => Exec["sync-overlays"];
+    define overlay() {
+        file {"/etc/overlays/${name}":
+            ensure  => file,
+            before  => Exec["sync-overlays"];
+        }
+    }
+
+    define makeconf_include() {
+        file {"/etc/make.conf.d/${name}.conf":
+            ensure  => present,
+            source  => "${modbase}/etc/make.conf.d/${name}.conf",
+            notify  => Exec["update-makeconf"];
+        }
     }
 }
+
