494bcbf67c126d06dd9b9c265f836588250464a4
Lance Albertson <lance@osuosl.org>
2010-09-18 18:26:46 -0700

    test for appended arrays of use flags

--- tests/package_use.pp
+++ tests/package_use.pp
@@ -1,17 +1,26 @@
-# test basic
-package_use { 'app-admin/puppet':
-    use_flags   => ['augeas', '-rrdtool'],
-    target      => "/tmp/test-package-use",
+class test {
+    # test basic
+    package_use { 'app-admin/puppet':
+        use_flags   => ['augeas', '-rrdtool'],
+        target      => "/tmp/test-package-use",
+    }
+
+    package_use { 'app-editors/vim':
+        use_flags   => "-perl",
+        target      => "/tmp/test-package-use",
+    }
+
+    # canonical name
+    package_use { 'mysql-client':
+        package     => "dev-db/mysql",
+        use_flags   => ['minimal', '-perl'],
+        target      => "/tmp/test-package-use",
+    }
 }
 
-package_use { 'app-editors/vim':
-    use_flags   => "-perl",
-    target      => "/tmp/test-package-use",
+class test::change inherits test {
+    Package_use["app-editors/vim"] { use_flags +> "perl", }
 }
 
-# canonical name
-package_use { 'mysql-client':
-    package     => "dev-db/mysql",
-    use_flags   => ['minimal', '-perl'],
-    target      => "/tmp/test-package-use",
-}
+#include test
+include test::change
