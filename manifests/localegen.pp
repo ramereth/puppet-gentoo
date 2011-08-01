0afef1d514b9e61d52d25b990fddd1c9f678339a
Lance Albertson <lance@osuosl.org>
2011-07-31 23:55:46 -0700

    Add locale.gen

---
+++ manifests/localegen.pp
@@ -0,0 +1,8 @@
+define gentoo::localegen ($value) {
+    config { "gentoo_localegen_${name}":
+        file    => "/etc/locale.gen",
+        line    => "$name $value",
+        pattern => "^$name $value$",
+        engine  => "replaceline",
+    }
+}
