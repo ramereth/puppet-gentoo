d792224b43600c24689fae4bd03aed6b4d3f4cfc
Lance Albertson <lance@osuosl.org>
2011-08-01 00:21:35 -0700

    Setup postsync files for portage

---
+++ manifests/postsync.pp
@@ -0,0 +1,12 @@
+define gentoo::postsync ($content="", $ensure=present) {
+    file {
+        "gentoo_postsync_${name}":
+            path    => "/etc/portage/postsync.d/${name}",
+            content => template("gentoo/postsync.sh.erb"),
+            mode    => 755,
+            ensure  => $ensure,
+            require => File["/etc/portage/postsync.d"];
+        "/etc/portage/postsync.d":
+            ensure  => directory;
+    }
+}
