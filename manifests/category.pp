809843fea89bb6ec3cbc79e6255fe15228538a68
Lance Albertson <lance@osuosl.org>
2011-07-31 23:50:10 -0700

    Gentoo category definition

---
+++ manifests/category.pp
@@ -0,0 +1,8 @@
+define gentoo::category {
+    config { "gentoo_category_${name}":
+        file    => "/etc/portage/categories",
+        line    => "$name",
+        pattern => "^$name$",
+        engine  => "replaceline",
+    }
+}
