fb1ee1a7dd20a767aa2c42d5909ecb5af892557f
Lance Albertson <lance@osuosl.org>
2011-07-31 23:42:46 -0700

    Move concat setup to init
    This should simplify this module a bit and rename the define to just
    gentoo::makeconf.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
 class gentoo {
+    include concat::setup
     # load all variables
     require gentoo::params
 
@@ -11,4 +12,17 @@ class gentoo {
     $gentoo_portdir_overlay = $gentoo::params::portdir_overlay
     $gentoo_accept_license  = $gentoo::params::accept_license
     $gentoo_emerge_opts = $gentoo::params::emerge_opts
+
+    # make.conf
+    concat { "/etc/make.conf":
+        owner => root,
+        group => root,
+        mode  => 644,
+    }
+
+    concat::fragment { "makeconf_base":
+        target  => $makeconf,
+        content => template("gentoo/makeconf.base.conf.erb"),
+        order   => 00,
+    }
 }
--- manifests/makeconf.pp
+++ manifests/makeconf.pp
@@ -1,22 +1,7 @@
-class gentoo::makeconf inherits gentoo {
+define gentoo::makeconf( $source="", $content="", comment="", $order=10,
+                         ensure=present ) {
     include concat::setup
-    $makeconf = "/etc/make.conf"
 
-    concat { $makeconf:
-        owner => root,
-        group => root,
-        mode  => 644,
-    }
-
-    concat::fragment { "makeconf_base":
-        target  => $makeconf,
-        content => template("gentoo/makeconf.base.conf.erb"),
-        order   => 00,
-    }
-}
-
-define gentoo::makeconf::setting( $source="", $content="", comment="",
-                                  $order=10, ensure=present ) {
     if $content {
         concat::fragment { "makeconf_$name":
             target  => "/etc/make.conf",
