b9a3870cf347fc3e8247d568a7c19cb075ba7fe4
Lance Albertson <lance@osuosl.org>
2011-08-11 16:48:57 -0700

    gentoo: switch to parameterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
-class gentoo {
+class gentoo (
+    $global_use  = $gentoo::params::global_use,
+    $features    = $gentoo::params::features,
+    $mirrors     = $gentoo::params::mirrors,
+    $sync        = $gentoo::params::sync,
+    $binhost     = $gentoo::params::binhost,
+    $portdir_overlay = $gentoo::params::portdir_overlay,
+    $accept_license  = $gentoo::params::accept_license,
+    $emerge_opts = $gentoo::params::emerge_opts
+    ) inherits gentoo::params {
+
     include concat::setup
     # load all variables
     require gentoo::params
-
-    # resets variables needed in templates
-    $gentoo_global_use  = $gentoo::params::global_use
-    $gentoo_features    = $gentoo::params::features
-    $gentoo_mirrors     = $gentoo::params::mirrors
-    $gentoo_sync        = $gentoo::params::sync
-    $gentoo_binhost     = $gentoo::params::binhost
-    $gentoo_portdir_overlay = $gentoo::params::portdir_overlay
-    $gentoo_accept_license  = $gentoo::params::accept_license
-    $gentoo_emerge_opts = $gentoo::params::emerge_opts
 
     # Add requires for Package provider
     Package {
