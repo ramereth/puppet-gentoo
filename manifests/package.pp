2875ec94b3f73c38f5a1e55eed027032d086aee7
Theo Chatzimichos <tampakrap@gentoo.org>
2012-11-24 23:09:34 +0100

    Add portage::package defined type
    Add package.pp module that defines a custom type gentoo::package which
    wraps up the built-in package and the package_* classes It supports
    unmask, mask, keywords, unified target, ensure package params, package
    version

---
+++ manifests/package.pp
@@ -0,0 +1,65 @@
+define portage::package (
+    $package = $title,
+    $version = undef,
+    $use = undef,
+    $use_target = undef,
+    $keywords = undef,
+    $keywords_target = undef,
+    $slot = undef,
+    $ensure = undef,
+    $target = undef,
+  ) {
+  if $version {
+    $package_full = "=$package-$version"
+    notify{ $package_full : }
+  }
+  else {
+    $package_full = $package
+  }
+  if $target {
+    if $use_target != undef {
+      $use_target = $target
+    }
+    if $keywords_target != undef {
+      $keywords_target = $target
+    }
+    if $unmask_target != undef {
+      $unmask_target = $target
+    }
+    if $mask_target != undef {
+      $mask_target = $target
+    }
+  }
+  if $keywords {
+    package_keywords { $package_full:
+      keywords => $keywords,
+      target   => $keywords_target,
+    }
+  }
+  if $unmask {
+    package_unmask { $package_full:
+    target => $unmask_target,
+    }
+  }
+  if $mask {
+    package_mask { $package_full:
+    target => $mask_target,
+    }
+  }
+  if $use {
+    package_use { $package:
+      use_flags => $use,
+      target    => $use_target,
+      notify    => Exec["changed_useflags"],
+    }
+    exec {"changed_useflags":
+      command     => "/usr/bin/emerge --reinstall=changed-use $package",
+      refreshonly => true,
+    }
+  }
+  package { $package:
+    ensure => $ensure,
+  }
+}
+
+# vim: set autoindent softtabstop=2 expandtab textwidth=80 shiftwidth=2:
