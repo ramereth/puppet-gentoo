8154b06a8f5b3893c7f0724f5b5347874d23f9e7
Adrien Thebo <git@somethingsinistral.net>
2012-11-26 11:46:07 -0800

    Improve documentation for atom validation

--- lib/puppet/util/portage.rb
+++ lib/puppet/util/portage.rb
@@ -1,12 +1,19 @@
-# module: Puppet::Util::Portage
-#
-# Contains DEPEND atom validation.
 module Puppet::Util::Portage
-  def self.valid_atom? atom
+  # Util methods for Portage types and providers.
+
+  # Determine if a string is a valid DEPEND atom
+  #
+  # @param atom [String] The string to validate as a DEPEND atom
+  #
+  # @return [TrueClass]
+  # @return [FalseClass]
+  #
+  # @see http://www.linuxmanpages.com/man5/ebuild.5.php#lbAE 'man 5 ebuild section DEPEND'
+  def self.valid_atom?(atom)
     atom_prefix  = %r{[<>=]|[<>]=}
     atom_name    = %r{[a-zA-Z-]+/[a-zA-Z-]+?}
     atom_version = %r{-[\d.]+[\w-]+}
-    
+
     base_atom = Regexp.new("^" + atom_name.to_s + "$")
     versioned_atom = Regexp.new("^" + atom_prefix.to_s + atom_name.to_s + atom_version.to_s + "$")
     depend = Regexp.union(base_atom, versioned_atom)
