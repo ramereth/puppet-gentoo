4e92a7b14bfb88a3855f02777b7f07feb1e68985
Adrien Thebo <git@somethingsinistral.net>
2012-11-26 11:12:39 -0800

    Remove voodoo programming from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,3 @@ RSpec.configure do |config|
   config.mock_with :mocha
 end
 
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
-end
