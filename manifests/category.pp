3add00c50c5dab2e3820ce0146ff45f58195c2f7
Adrien Thebo <git@somethingsinistral.net>
2012-11-26 12:18:44 -0800

    Remove gentoo::category
    /etc/portage/categories is no longer used.

--- manifests/category.pp
+++
@@ -1,8 +0,0 @@
-define gentoo::category {
-  config { "gentoo_category_${name}":
-    file  => "/etc/portage/categories",
-    line  => "$name",
-    pattern => "^$name$",
-    engine  => "replaceline",
-  }
-}
