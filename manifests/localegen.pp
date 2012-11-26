83badc350bc6b44370ceb4df62f4a0bb3fe33b66
Adrien Thebo <git@somethingsinistral.net>
2012-11-26 12:25:17 -0800

    Remove gentoo::localegen
    /etc/locale.gen is a pretty static file, and the `config` defined type
    is not available so this defined type is not useful, nor functional.

--- manifests/localegen.pp
+++
@@ -1,8 +0,0 @@
-define gentoo::localegen ($value) {
-  config { "gentoo_localegen_${name}":
-    file  => "/etc/locale.gen",
-    line  => "$name $value",
-    pattern => "^$name $value$",
-    engine  => "replaceline",
-  }
-}
